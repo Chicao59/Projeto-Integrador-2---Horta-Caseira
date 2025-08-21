1c827f069374dd2cb09bbc71b5dd8192b6be01db
Chicao59 <francisco.machado@ufms.br>
2025-08-21 09:21:07 -0400

    Update Horta Caseira.php

--- Horta Caseira.php
+++ Horta Caseira.php
@@ -6,11 +6,9 @@ $dbname = "HortaCaseira";
 
 // Criando conexão
 $conn = new mysqli($servername, $username, $password, $dbname);
-
 // Verificar a conexão
 if ($conn->connect_error) {
     die("Falha na conexão: " . $conn->connect_error);
 }
-
 echo "Conectado com sucesso!";
 ?>
