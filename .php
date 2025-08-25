36e18b9fe872f99f4e55882dd781f93474e6cbe1
Chicao59 <francisco.machado@ufms.br>
2025-08-25 12:53:39 -0400

    Horta_Caseira.php

---
+++ .php
@@ -0,0 +1,13 @@
+<?php
+$servername = "root@localhost";
+$username = "root";
+$password = "";
+$dbname = "HortaCaseira";
+// Criando conexão
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Verificar a conexão
+if ($conn->connect_error) {
+    die("Falha na conexão: " . $conn->connect_error);
+}
+echo "Conectado!";
+?>
